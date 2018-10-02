99d481e2b91ec81fe2847e868c20a6cce9ed3074
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 19:24:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 
+  Handlebars.registerHelper('displayIngredient', function() {
+    if(this.state === "closed") {
+      return new Handlebars.SafeString(this.body)
+    } else {
+      return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+    }
+  })
+
   function renderMain() {
     let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
     let html = template({name: 'Gordon Ramsay'});
