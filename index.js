a6eefba3f46798c27a635a1406e02da13a9604c7
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 19:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 
   Handlebars.registerHelper('displayIngredient', function() {
-    if(this.state === "closed") {
-      return new Handlebars.SafeString(this.body)
-    } else {
-      return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
-    }
+      return new Handlebars.SafeString(this.body);
   })
 
   function renderMain() {
