7e2fd77ba453e9d03e1916e91f24c7a31e1e6a80
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 18:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("partial-template").innerHTML)
   function renderMain() {
     var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
     var html = template({name: 'Gordon Ramsay'});
