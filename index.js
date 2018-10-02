58c370a0aae4bf8721be6f33a7a04021818f1d00
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-02 18:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('namePartial', document.getElementById("partial-template").innerHTML)
+  function renderMain() {
+    var template = Handlebars.compile(document.getElementById("main-template").innerHTML);
+    var html = template({name: 'Gordon Ramsay'});
+  }
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
