6e6f42a78e6e6a8f29fef97f4184be1d03ebb807
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-03 14:02:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial('recipeFormPartial')
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
 
   Handlebars.registerHelper('displayIngredient', function() {
       return new Handlebars.SafeString(this.body);
